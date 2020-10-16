5c9c101a45bf8df1a525fe6b24aaa085bb8e7571
unknown <TEMPUSER@wanckes.nl>
2020-10-16 12:49:46 +0200

    Added additional files

--- Scripts/AD/Bulk user import/bulk-user-import.ps1
+++ Scripts/AD/Bulk user import/bulk-user-import.ps1
@@ -2,29 +2,31 @@
 Import-Module activedirectory
   
 #Store the data from ADUsers.csv in the $ADUsers variable
-$ADUsers = Import-csv C:\Users\TEMPUSER\Documents\bulk-user-import.csv
+$ADUsers = Import-csv 'C:\Users\TEMPUSER\Documents\Bulk user import\bulk-user-import.csv'
 
 #Loop through each row containing user details in the CSV file 
 foreach ($User in $ADUsers)
 {
 	#Read user data from each field in each row and assign the data to a variable as below
 		
-	$Username 	= $User.username
-	$Password 	= $User.password
-	$Firstname 	= $User.firstname
-	$Lastname 	= $User.lastname
-	$OU 		= $User.ou #This field refers to the OU the user account is to be created in
-    $email      = $User.email
-    $streetaddress = $User.streetaddress
-    $city       = $User.city
-    $zipcode    = $User.zipcode
-    $state      = $User.state
-    $country    = $User.country
-    $telephone  = $User.telephone
-    $jobtitle   = $User.jobtitle
-    $company    = $User.company
-    $department = $User.department
+	$Username 	= $User.Username
+	$Password 	= $User.Password
+	$Firstname 	= $User.GivenName
+	$MiddleInitial = $User.MiddleInitial
+	$Lastname 	= $User.Surname
+	$OU 		= "OU=Users,OU=Accounts,DC=wanckes,DC=nl"
+    $email      = $User.Email
+    $streetaddress = $User.StreetAddress
+    $city       = $User.City
+    $zipcode    = $User.ZipCode
+    $state      = $User.StateFull
+    $country    = $User.CountryFull
+    $telephone  = $User.TelephoneNumber
+    $jobtitle   = $User.JobTitle
+    $company    = $User.Company
+    $department = $User.Department
     $Password = $User.Password
+	$Memberoff = $User.MemberOff
 
 
 	#Check to see if the user already exists in AD
@@ -38,7 +40,7 @@ foreach ($User in $ADUsers)
 		#User does not exist then proceed to create the new user account
 		
         #Account will be created in the OU provided by the $OU variable read from the CSV file
-		New-ADUser -SamAccountName $Username -UserPrincipalName "$Username@wanckes.nl" -Name "$Firstname $Lastname" -GivenName $Firstname -Surname $Lastname -Enabled $True -DisplayName "$Lastname, $Firstname" -Path $OU -City $city -Company $company -State $state -StreetAddress $streetaddress -OfficePhone $telephone -EmailAddress $email -Title $jobtitle -Department $department -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -ChangePasswordAtLogon $True
-            
+        New-ADUser -SamAccountName "$Username" -UserPrincipalName "$Username@wanckes.nl" -Name "$Firstname $Lastname" -GivenName "$Firstname" -Surname "$Lastname" -Initials "$MiddleInitial" -EmailAddress "$email" -Enabled $True -DisplayName "$Lastname, $Firstname" -Path $OU -City "$city" -Country "$country" -PostalCode "$zipcode" -Company "$company" -State "$state" -StreetAddress "$streetaddress" -OfficePhone "$telephone" -Title "$jobtitle" -Department "$department" -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -ChangePasswordAtLogon $True -ProfilePath "\\wanckes.nl\Utility\profiles$\%username%" -HomePage "https://www.wanckes.nl"
+        Add-ADGroupmember -Members "$Username" -Identity "$Memberoff"  
 	}
 }
